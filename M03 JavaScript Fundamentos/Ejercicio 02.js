77b781fb48f028872f71b339e717833fb9d66def
Jose Lodi <cpjoselodi.emp@gmail.com>
2023-02-25 01:46:01 -0300

    Tarea 4

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -4,6 +4,7 @@
 function devolverString(string) {
    // Debe retornar un string.
    // Tu código:
+   return string;
 }
 
 // ⛔️ "X" e "Y" son números.
@@ -11,26 +12,41 @@ function devolverString(string) {
 function suma(x, y) {
    // Retorna el resultado de su suma.
    // Tu código:
+   var resultado = 0;
+   resultado = x + y;
+   return resultado;
 }
 
 function resta(x, y) {
    // Retorna el resultado de la resta.
    // Tu código:
+   var resultadoResta = 0;
+   resultadoResta = x - y;
+   return resultadoResta;
 }
 
 function divide(x, y) {
-   // Retorna el resultado de su división.
+   // Retorna el resultado de la division.
    // Tu código:
+   var resultadoDivision = 0;
+   resultadoDivision = x / y;
+   return resultadoDivision; 
 }
 
 function multiplica(x, y) {
    // Retorna el resultado de su multiplicación.
    // Tu código:
+   var resultadoMultiplicacion = 0;
+   resultadoMultiplicacion = x * y;
+   return resultadoMultiplicacion;
 }
 
 function obtenerResto(x, y) {
    // Obten el resto de la división de "x" entre "y".
    // Tu código:
+   var RestoDivision = 0;
+   RestoDivision = x % y;
+   return RestoDivision;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
--- M03 JavaScript Fundamentos/Ejercicio 04.js
+++ M03 JavaScript Fundamentos/Ejercicio 04.js
@@ -6,31 +6,43 @@
 function elevarAlCuadrado(num) {
    // Retorna el valor de "num" elevado al cuadrado.
    // Tu código:
+   return Math.pow(num,2);
 }
 
 function elevarAlCubo(num) {
    // Retorna el valor de "num" elevado al cubo.
    // Tu código:
+   return Math.pow(num,3);
 }
 
 function elevar(num, exponent) {
    // Retorna el valor de "num" elevado al exponente "exponent".
    // Tu código:
+   return Math.pow(num,exponent);
 }
 
 function redondearNumero(num) {
    // Redondea "num" al entero más próximo y retórnalo.
    // Tu código:
+   var redondeo = 0;
+   redondeo = Math.round(num);
+   return redondeo;
 }
 
 function redondearHaciaArriba(num) {
    // Redondea "num" hacia arriba y retórnalo.
    // Tu código:
+   var redondeoArriba = 0;
+   redondeoArriba = Math.ceil(num);
+   return redondeoArriba;
 }
 
 function numeroRandom() {
    // Genera un número al azar entre 0 y 1 y retórnalo.
    // Tu código:
+   var azar = 0;
+   azar = Math.random();
+   return azar;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
